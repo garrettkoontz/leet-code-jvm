d2c8916d9341a37fff902e7ea1b36cfa3108693c
garrett.koontz <garrett.koontz@wework.com>
2019-11-12 17:14:57 -0500

    WIP palindrome 4

---
+++ src/main/java/com/k00ntz/euler/LargestPalindromeProduct4.java
@@ -0,0 +1,44 @@
+package com.k00ntz.euler;
+
+import java.util.List;
+
+/**
+ * A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+ * <p>
+ * Find the largest palindrome made from the product of two 3-digit numbers.
+ */
+public class LargestPalindromeProduct4 {
+
+    static int maxPalindrome(int min, int max) {
+        int currentMaxPalindrome = Integer.MIN_VALUE;
+        for (int i = max; i >= min; i--) {
+            for (int j = max; j >= min; j--) {
+                int t = i * j;
+                if (t > currentMaxPalindrome) {
+                    if (isPalindrome(t)) currentMaxPalindrome = t;
+                }
+            }
+        }
+        return currentMaxPalindrome;
+    }
+
+    static boolean isPalindrome(int i) {
+        double log = Math.floor(Math.log10(i));
+        int highIndex = (int) Math.pow(10, log);
+        int lowIndex = 10;
+        while (highIndex >= lowIndex) {
+            int modHigh = (i % (highIndex * 10)) / highIndex;
+            int modLow = (i / (lowIndex / 10)) % lowIndex;
+            if (modHigh != modLow) return false;
+            highIndex /= 10;
+            lowIndex *= 10;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(9009));
+        System.out.println(maxPalindrome(10, 99));
+        System.out.println(maxPalindrome(100, 999));
+    }
+}
--- src/main/java/com/k00ntz/euler/LargestPrimeFactor3.java
+++ src/main/java/com/k00ntz/euler/LargestPrimeFactor3.java
@@ -5,6 +5,12 @@ import java.util.Collections;
 import static com.k00ntz.euler.util.ListUtil.last;
 import static com.k00ntz.euler.util.Primes.primeFactors;
 
+
+/**
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ *
+ * What is the largest prime factor of the number 600851475143 ?
+ */
 public class LargestPrimeFactor3 {
 
     public static void main(String[] args) {
