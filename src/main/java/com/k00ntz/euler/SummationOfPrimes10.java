1ae99b62e290149a5f2eda37a40b236d7e1551af
garrett.koontz <garrett.koontz@wework.com>
2019-11-13 16:04:47 -0500

    sum of primes

---
+++ src/main/java/com/k00ntz/euler/SummationOfPrimes10.java
@@ -0,0 +1,23 @@
+package com.k00ntz.euler;
+
+import com.k00ntz.euler.util.Primes;
+
+import java.util.List;
+
+/**
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ *
+ * Find the sum of all the primes below two million.
+ */
+public class SummationOfPrimes10 {
+
+    public static long sumOfPrimesBelow(int i){
+        List<Integer> primes = Primes.segmentedSieve(i);
+        return primes.stream().mapToLong(x -> (long)x).reduce(Long::sum).orElse(-1L);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumOfPrimesBelow(10));
+        System.out.println(sumOfPrimesBelow(2000000));
+    }
+}
