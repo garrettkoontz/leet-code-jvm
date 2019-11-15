722dbf654d0ad2ce30efee1c3289c62875abf745
gedekay <gedekay@gmail.com>
2019-11-14 22:17:42 -0500

    collatz

--- src/main/java/com/k00ntz/euler/LongestCollatzSequence14.java
+++ src/main/java/com/k00ntz/euler/LongestCollatzSequence14.java
@@ -1,52 +1,39 @@
 package com.k00ntz.euler;
 
-import java.util.Iterator;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LongestCollatzSequence14 {
 
-    private static class CollatzIter implements Iterator<Integer> {
+    public static Map<Long, Long> collatzMap = new HashMap<Long, Long>() {{
+        put(1L, 1L);
+    }};
 
-        private int internalNum;
-
-        public CollatzIter(int startNum) {
-            startNum = internalNum;
-        }
-
-        @Override
-        public boolean hasNext() {
-            return internalNum > 1;
-        }
-
-        @Override
-        public Integer next() {
-            internalNum = internalNum % 2 == 0 ? internalNum / 2 : 3 * internalNum + 1;
-            return internalNum;
-        }
+    public static long nextCollatz(long input) {
+        return input % 2 == 0 ? input / 2 : 3 * input + 1;
     }
 
-    public static int countCollatz(int i) {
-        int j = 0;
-        CollatzIter iter = new CollatzIter(i);
-        for (; iter.hasNext(); ) {
-            iter.next();
-            j++;
+    public static long longestCollatz(long maxStartExclusive) {
+        for (int i = 1; i < maxStartExclusive; i++) {
+            lengthOfCollatzFrom(i, collatzMap);
         }
-        return j;
+        Map.Entry<Long, Long> maxEntry = collatzMap.entrySet().stream().filter(x -> x.getKey() < maxStartExclusive)
+                .max(Comparator.comparing(Map.Entry::getValue)).get();
+        return maxEntry.getKey();
     }
 
-
-    public static class CollatzCounter implements Runnable {
-        public CollatzCounter(int i) {
-
+    public static long lengthOfCollatzFrom(long i, Map<Long, Long> entries) {
+        if (entries.containsKey(i)) return entries.get(i);
+        else {
+            long collatzLength = 1 + lengthOfCollatzFrom(nextCollatz(i), entries);
+            entries.put(i, collatzLength);
+            return collatzLength;
         }
-    }
-
-    public static int longestCollatz(int maxStart) {
-
     }
 
     public static void main(String[] args) {
-
+        System.out.println(longestCollatz(1000000));
     }
 
 }
