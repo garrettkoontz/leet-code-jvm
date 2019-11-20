6b271df8fd0db8669237f2070d2652a67686911c
garrett.koontz <garrett.koontz@wework.com>
2019-11-20 11:41:21 -0500

    digit factorials

--- src/main/java/com/k00ntz/euler/util/NumberUtil.java
+++ src/main/java/com/k00ntz/euler/util/NumberUtil.java
@@ -5,6 +5,21 @@ import java.util.List;
 
 public class NumberUtil {
 
+    private static List<Long> factorialCache = new ArrayList<Long>() {{
+        this.add(1L);
+        this.add(1L);
+    }};
+
+    public static Long factorial(int i) {
+        if (factorialCache.size() > i) {
+            return factorialCache.get(i);
+        } else {
+            long fact = ((long) i) * factorial(i - 1);
+            factorialCache.add(fact);
+            return fact;
+        }
+    }
+
     public static List<Integer> getDigits(int i) {
         List<Integer> digits = new ArrayList<>();
         for (char c : ("" + i).toCharArray()) {
