c03ec6bb20beafd6a864a805e540ca1d0043e730
gedekay <gedekay@gmail.com>
2019-11-21 08:21:55 -0500

    coded triangle numbers

--- src/main/java/com/k00ntz/euler/CodedTriangleNumbers42.java
+++ src/main/java/com/k00ntz/euler/CodedTriangleNumbers42.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 
 public class CodedTriangleNumbers42 {
 
@@ -22,5 +25,24 @@ public class CodedTriangleNumbers42 {
         }
     }
 
+    public static long triangleFor(long n) {
+        return n * (n + 1) / 2;
+    }
 
+    public static Set<Long> triangleNumbers = LongStream.rangeClosed(1, 1000)
+            .map(CodedTriangleNumbers42::triangleFor).boxed().collect(Collectors.toSet());
+
+    public static boolean isTriangleWord(String s) {
+        long sum = 0;
+        for (char c :
+                s.toCharArray()) {
+            sum += c - 'A' + 1;
+        }
+        return triangleNumbers.contains(sum);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isTriangleWord("SKY"));
+        System.out.println(words.stream().filter(CodedTriangleNumbers42::isTriangleWord).count());
+    }
 }
